d17187e9f72d1e6708cd6fe2de912b3f4fb2ad1c
danirisdiandita <normadani2016@gmail.com>
2026-03-06 15:57:14 +0700

    Update data 2026-03-06 15:57:14

--- push.sh
+++ push.sh
@@ -1,5 +1,10 @@
-#/bin/bash
-date=$(date +"%Y-%m-%d %H:%M:%S")
+#!/bin/bash
+set -e
+
+# Generate timestamp
+current_date=$(date +"%Y-%m-%d %H:%M:%S")
+
+# Push changes
 git add . 
-git commit -m 'Update data $date'
+git commit -m "Update data $current_date"
 git push
